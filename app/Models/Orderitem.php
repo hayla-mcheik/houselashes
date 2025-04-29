3ad708bbabd4a4b9d6eaab1de48aebbdc42ae2af
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2025-04-29 10:12:45 +0300

    first commit

--- app/Models/Orderitem.php
+++ app/Models/Orderitem.php
@@ -7,7 +7,7 @@ use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-class Orderitem extends Model
+class OrderItem extends Model
 {
     use HasFactory;
     protected $table='orders_items';
