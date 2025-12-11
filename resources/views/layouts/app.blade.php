703c283252a13f6871cac7ff4faee949efc0aed0
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2025-12-11 16:07:56 +0200

    lakanto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>House lashes | Laravel Ecommerce</title>
+    <title>Lakanto | Laravel Ecommerce</title>
 
     <!--== Favicon ==-->
     <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
